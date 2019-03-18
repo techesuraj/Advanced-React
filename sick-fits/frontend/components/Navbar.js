718c066e47033cc6f9703b9f6ac91ca500a76be6
Suraj <suraj.gantedi@gmail.com>
2019-03-17 23:33:29 -0400

    Added styles and fixed flicker with _document

--- sick-fits/frontend/components/Navbar.js
+++ sick-fits/frontend/components/Navbar.js
@@ -1,13 +1,12 @@
 import Link from 'next/link';
-
+import NavStyles from './styles/NavStyles'
 const Navbar = () => (
-    <div>
-        <Link href="/">
-            <a>Home</a>
-        </Link>
-        <Link href="/sell">
-            <a>Sell</a>
-        </Link>
-    </div>
+    <NavStyles>
+        <Link href="/items">Items</Link>
+        <Link href="/sell">Sell</Link>
+        <Link href="/signup">Signup</Link>
+        <Link href="/orders">Orders</Link>
+        <Link href="/account">Account</Link>
+    </NavStyles>
 )
 export default Navbar;
